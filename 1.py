702a6d9955417b61a2c777203e182a3c080ba87b
David <davidrabbit28@gmail.com>
2025-11-06 12:33:33 +0300

    add function sumnames

--- 1.py
+++ 1.py
@@ -6,4 +6,13 @@ def onlynames(names):
             names_on_d.append(i)
     return names_on_d
 
+def sumnames(names):
+    sum = 0
+    for i in names:
+        sum += len(i)
+    return sum
+
+
+
 print(onlynames(names))
+print(sumnames(names))
