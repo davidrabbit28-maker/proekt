fe3397743c6e5f5b22081f981103daab19d69141
David <davidrabbit28@gmail.com>
2025-12-01 15:49:59 +0300

    add main while

--- to_do/app.py
+++ to_do/app.py
@@ -3,8 +3,6 @@ class task:
         self.priority = priority
         self.status = False
         self.description = description
-    def test(self):
-        return self.priority, self.status
 
     def to_dict(self):
         slovar = {"Описание":self.description, 
@@ -25,10 +23,11 @@ class task:
         return self.priority
 
 
-a = task("Помыть посуду", "Важно")
-print(a.change_status())
-print(a.change_priority("Очень важно"))
-print(a.test())
+    def show(self):
+        if self.status:
+            print(f"Выполнено! {self.description}")
+        else:
+            print(f"Не выполнено! {self.description}")
 
 
 class to_do_list:
@@ -38,11 +37,32 @@ class to_do_list:
     def add_task(self, description):
         t = task(description)
         self.spisok.append(t)
+        print("Задача добавлена.")
 
-    def test(self):
-        return self.spisok
+    def show_all_tasks(self):
+        for i, task in enumerate(self.spisok, 1):
+            print(i)
+            task.show()
+#  
+# 
+# 
+# 
+# 
+print("Мой список дел.")
+ 
+to_do = to_do_list()
 
-
-b = to_do_list()
-print(b.add_task("Приготовить омлет"))
-print(b.test())
+while True:
+    print("1 - добавить, 2 - посмотреть весь список, 3 - выйти")
+    ask = int(input("Введите цифру: "))
+    if ask == 1:
+        desc_ask = input("Введите задачу: ")
+        to_do.add_task(desc_ask)
+        
+    elif ask == 2:
+        to_do.show_all_tasks()
+    elif ask == 3:
+        print("Вы вышли.")
+        break
+    else:
+        print("Вы не ввели нужную цифру!")
